26abe4e9ebc7b96c1fd5f9efeb3c7bb0987eade1
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-03-11 10:03:03 -0300

    Carregando detalhe do Item sem a Localização.

--- app/src/main/java/com/exemple/meuprimeiroapp/ItemDetailActivity.kt
+++ app/src/main/java/com/exemple/meuprimeiroapp/ItemDetailActivity.kt
@@ -3,12 +3,80 @@ package com.exemple.meuprimeiroapp
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.exemple.meuprimeiroapp.databinding.ActivityItemDetailBinding
+import com.exemple.meuprimeiroapp.model.Item
+import com.exemple.meuprimeiroapp.service.RetrofitClient
+import com.exemple.meuprimeiroapp.service.safeApiCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.exemple.meuprimeiroapp.service.Result
+import com.exemple.meuprimeiroapp.ui.loadUrl
+
 
 class ItemDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityItemDetailBinding
+
+    private lateinit var item: Item
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_item_detail)
+        binding = ActivityItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        SetupViews()
+        loadItem()
+
+
+    }
+    private fun SetupViews() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+    private fun loadItem() {
+        val itemId = intent.getStringExtra(ARG_ID) ?: ""
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = RetrofitClient.itemApiService.getItems(itemId)
+            safeApiCall { result }
+
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Success<*>  -> {
+                        item = result.data as Item
+                        handleOnSuccess()
+                    }
+
+                    is Result.Error -> {
+                        Toast.makeText(
+                            this@ItemDetailActivity,
+                            "Erro ao buscar o getItem(itemId)",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun handleOnSuccess() {
+        binding.name.text = item.value.fullName
+        binding.age.text = getString(R.string.item_age, item.value.age)
+        binding.profession.setText(item.value.profession)
+        binding.image.loadUrl(item.value.imageUrl)
 
     }
 
--- app/src/main/java/com/exemple/meuprimeiroapp/service/ItemApiService.kt
+++ app/src/main/java/com/exemple/meuprimeiroapp/service/ItemApiService.kt
@@ -2,10 +2,14 @@ package com.exemple.meuprimeiroapp.service
 
 import com.exemple.meuprimeiroapp.model.Item
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ItemApiService {
     @GET("items")
     suspend fun getItems(): List<Item>
+
+    @GET("items/{id}")
+    suspend fun getItems(@Path("id") id: String): Item
 }
 
 
