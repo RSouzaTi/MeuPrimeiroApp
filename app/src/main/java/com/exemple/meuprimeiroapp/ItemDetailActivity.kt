9c81b3ca62104d800a8c3475bb33172fc68cf973
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-03-11 19:23:13 -0300

    Carregar item no Mapa caso loadItem tenha delay

--- app/src/main/java/com/exemple/meuprimeiroapp/ItemDetailActivity.kt
+++ app/src/main/java/com/exemple/meuprimeiroapp/ItemDetailActivity.kt
@@ -23,41 +23,38 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-
 class ItemDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityItemDetailBinding
-
-    private lateinit var item: Item
-
-    private lateinit var mMap: GoogleMap
-
-
-
+    private var item: Item? = null
+    private var mMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        SetupViews()
+        setupViews()
         loadItem()
         setupGoogleMap()
-
-
     }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-    }
-    private fun SetupViews() {
+       // if (::item.isInitialized) {
+       // Se  o item já estiver carregado, carregueo no mapa
+            loadItemInGoogleMap()
+        }
+
+
+    private fun setupViews() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
-
     }
+
     private fun loadItem() {
         val itemId = intent.getStringExtra(ARG_ID) ?: ""
 
@@ -69,50 +66,51 @@ class ItemDetailActivity : AppCompatActivity(), OnMapReadyCallback {
                         item = result.data
                         handleSuccess()
                     }
+
                     is Result.Error -> {
-                            Toast.makeText(
+                        Toast.makeText(
                             this@ItemDetailActivity,
-                            "Erro ao buscar o getItem(itemId)",
+                            "Erro ao buscar o item",
                             Toast.LENGTH_SHORT
-                            ).show()
+                        ).show()
                     }
                 }
             }
         }
     }
 
-    private fun setupGoogleMap(){
+    private fun setupGoogleMap() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync (this)
+        mapFragment.getMapAsync(this)
     }
 
     private fun handleSuccess() {
-        binding.name.text = item.value.fullName
-        binding.age.text = getString(R.string.item_age, item.value.age)
-        binding.profession.setText(item.value.profession)
-        binding.image.loadUrl(item.value.imageUrl)
-        loadMapInGoogleMap()
+        val currentItem = item ?: return
+        binding.name.text = currentItem.value.fullName
+        binding.age.text = getString(R.string.item_age, currentItem.value.age)
+        binding.profession.setText(currentItem.value.profession)
+        binding.image.loadUrl(currentItem.value.imageUrl)
 
+        loadItemInGoogleMap()
     }
 
-    private fun loadMapInGoogleMap(){
-        item.value.location?.let {
+    private fun loadItemInGoogleMap() {
+        item?.value?.location?.let {
             binding.googleMapContent.visibility = View.VISIBLE
             val location = LatLng(it.latitude, it.longitude)
-            mMap.addMarker(
+            mMap?.addMarker(
                 MarkerOptions()
-                .position(location)
-                .title(it.name)
+                    .position(location)
+                    .title(it.name)
             )
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                location,
-                15f
-            ))
-
+            mMap?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    15f
+                )
+            )
         }
-
-   }
-
+    }
 
     companion object {
         const val ARG_ID = "arg_id"
